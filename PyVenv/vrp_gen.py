7d44d202320a28214964106bc3f4045f71ad01be
David Zeman <david.zeman@outlook.com>
2022-11-22 19:32:16 +0100

    vrp gen

---
+++ PyVenv/vrp_gen.py
@@ -0,0 +1,19 @@
+
+
+"""
+Returns instance of vehicle routing problem
+"""
+def create_vrp():
+    return None    
+
+"""
+Returns instance of capacitated vrp
+"""
+def create_cvrp():
+    return None  
+
+"""
+Returns instance of capacitated vrp with time windows
+"""
+def create_cvrptw():
+    return None  
